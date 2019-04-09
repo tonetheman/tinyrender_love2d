051531d463f917c981aa037626e52bfa538cd95d
Tony Colston <tonetheman@gmail.com>
2019-04-09 11:02:15 -0500

    parsing to points array

--- parser.lua
+++ parser.lua
@@ -1,5 +1,14 @@
 
 
+local Points = {}
+function Points.new(a,b,c)
+    local self = {}
+    self.a = a
+    self.b = b
+    self.c = c
+    return self
+end
+
 function file_exists(file)
     local f = io.open(file,"rb")
     if f then f:close() end
@@ -15,21 +24,37 @@ function lines_from(file)
     return lines
 end
 
-local file = "african_head.obj"
-local lines = lines_from(file)
-for k,v in pairs(lines) do
-    local res = string.find(v,"v ")
-    if res ~= nil then
-        print("\tres " .. res)
-    end
-    if res == 1 then
-        print("line: " .. k .. " -> " .. v)
-    end
+function get_v_line(s)
+    -- print("-------------")
+    -- local s = "v -0.378028 -0.321668 -0.326088"
+    a,b,c,d = string.match(s,"(v%s+(-?%d+[.]%d+)%s+(-?%d+[.]%d+)%s+(-?%d+[.]%d+))")
+    -- print("b " ..b)
+    -- print("c " ..c)
+    -- print("d " ..d)
+    return tonumber(b),tonumber(c),tonumber(d)
 end
 
-print("-------------")
-local s = "v -0.378028 -0.321668 -0.326088"
-a,b,c,d = string.match(s,"(v%s+(-?%d+[.]%d+)%s+(-?%d+[.]%d+)%s+(-?%d+[.]%d+))")
-print("b " ..b)
-print("c " ..c)
-print("d " ..d)
+function get_points()
+    local file = "african_head.obj"
+    local lines = lines_from(file)
+    local pts = {}
+    for k,v in pairs(lines) do
+        local res = string.find(v,"v ")
+        if res ~= nil then
+            -- print("\tres " .. res)
+        end
+        if res == 1 then
+            -- print("line: " .. k .. " -> " .. v)
+            local a,b,c = get_v_line(v)
+            pts[#pts+1] = Points.new(a,b,c)
+        end
+    end
+    return pts
+end
+
+-- local s = "v -0.378028 -0.321668 -0.326088"
+-- print(get_v_line(s))
+local pts = get_points()
+for k,v in pairs(pts) do
+    print(v.a,v.b,v.c)
+end
